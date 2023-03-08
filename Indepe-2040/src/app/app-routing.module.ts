37b8d3b22b8f236b4440fe596ba15ceb66086eb7
MaribelAlvarez01 <a00828510@itesm.mx>
2023-03-08 10:13:43 -0600

    login v1

--- Indepe-2040/src/app/app-routing.module.ts
+++ Indepe-2040/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
+import { LoginComponent } from './views/login/login/login.component';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginGuard } from './guards/login.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent, canActivate: [LoginGuard]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
