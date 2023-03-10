21999b156e56c8cfe7dea3207cff1dd306d35151
MaribelAlvarez01 <a00828510@itesm.mx>
2023-03-10 08:26:38 -0600

    PPP

--- Indepe-2040/src/app/views/registro/forms-registro/forms-registro.component.ts
+++ Indepe-2040/src/app/views/registro/forms-registro/forms-registro.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { LocationService } from 'src/app/services/location.service';
 
 @Component({
@@ -7,10 +7,31 @@ import { LocationService } from 'src/app/services/location.service';
   templateUrl: './forms-registro.component.html',
   styleUrls: ['./forms-registro.component.css']
 })
-export class FormsRegistroComponent {
-  firstFormGroup = this._formBuilder.group({
-    nombre: ['', Validators.required],
-  });
+export class FormsRegistroComponent implements OnInit{
+  datosGeneralesInfanteFormGroup !: FormGroup;
+  firstFormGroup !: FormGroup;
+
+  ngOnInit(): void {
+    this.datosGeneralesInfanteFormGroup = this._formBuilder.group({
+      nombre: [null, Validators.required],
+      apellido_p: [null, Validators.required],
+      apellido_m: [null, Validators.required],
+      CURP: [null, Validators.required],
+      foto: [null, Validators.required],
+      persona_2040: [null, Validators.required],
+      fecha_nacimiento: [null, Validators.required],
+      fecha_tentativa_nacimiento: [null, Validators.required],
+      canal_difusion_plan: [null, Validators.required],
+      sexo: [null, Validators.required],
+      telefono: [null, Validators.required],
+      estatus_acomp: [null, Validators.required],
+      responsable: [null, Validators.required],
+      seguro_medico: [null, Validators.required],
+    });
+    this.firstFormGroup = this._formBuilder.group({
+      nombre: [null, Validators.required],
+    });
+  }
   country = new FormControl(null, [Validators.required]);
   state = new FormControl({ value: null, disabled: true }, [
     Validators.required,
